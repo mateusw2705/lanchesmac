c5e8decac5e9b427556a67d22371e4e772f01c25
mateusw2705 <mateuswagner2705@gmail.com>
2024-08-22 19:36:15 -0300

    complemento projeto lanches mac

--- LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625f06a65c3594bd26e6aa2954abc1e705220fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
